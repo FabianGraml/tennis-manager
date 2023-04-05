0eced0fc0408f3745cddf5028f7eec5cb613b816
FabianGraml <fabian.graml03@gmail.com>
2023-04-05 08:39:13 +0200

    update

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { AddBookingComponent } from './components/add-booking/add-booking.component';
 
 
 
 @NgModule({
   declarations: [
   
-    HeaderComponent
+    HeaderComponent,
+       AddBookingComponent
   ],
   imports: [
     CommonModule
